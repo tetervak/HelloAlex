a55552bb02a3faa5ddedab9d10de392a0d495cc2
Alex Tetervak <tetervak@sheridancollege.ca>
2021-01-30 19:36:24 -0500

    added the log message

--- app/src/main/java/ca/tetervak/helloalex/MainActivity.kt
+++ app/src/main/java/ca/tetervak/helloalex/MainActivity.kt
@@ -2,10 +2,17 @@ package ca.tetervak.helloalex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val TAG = "Hello Alex"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "Alex Message 1")
     }
 }
